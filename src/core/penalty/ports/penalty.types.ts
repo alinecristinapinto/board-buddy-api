c64a2c90edc6aa8e832a58bbbbe44a31cffbd10a
Aline Cristina Pinto <alinecristina3.14nto@gmail.com>
2024-06-03 19:59:05 -0300

    fixing penalty type

--- src/core/penalty/ports/penalty.types.ts
+++ src/core/penalty/ports/penalty.types.ts
@@ -6,4 +6,4 @@ export type Penalty = {
 
 export type AddPenalty = Pick<Penalty, 'loan_id'>;
 export type PayPenalty = Pick<Penalty, 'loan_id'> & { profile_id: string };
-export type UpdatePenalty = Omit<Penalty, 'created_at'>;
+export type UpdatePenalty = Pick<Penalty, 'loan_id'> & { payed_at: Date };
